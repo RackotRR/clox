8879c23416198dacd396de319a4e98b18403eaff
AsrankulovAleksandr <Asrankulov@benice.games>
2023-03-25 19:03:12 +0300

    add nil and boolean types

--- vm/src/vm.c
+++ vm/src/vm.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "common.h"
@@ -10,6 +11,20 @@ static VM vm;
 static void resetStack() {
     vm.stack_top = vm.stack;
 }
+
+static void runtimeError(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fputs("\n", stderr);
+
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line = getLine(&vm.chunk->lines_info, instruction);
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack();
+}
+
 void initVM() {
     resetStack();
 }
@@ -27,14 +42,25 @@ Value pop() {
     return *vm.stack_top;
 }
 
+static Value peek(int distance) {
+    return vm.stack_top[-1 - distance];
+}
+static bool isFalsey(Value value) {
+    return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op) \
+#define BINARY_OP(value_type, op) \
     do { \
-        double b = pop(); \
-        double a = pop(); \
-        push(a op b); \
+        if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+            runtimeError("Operands must be numbers."); \
+            return INTERPRET_RUNTIME_ERROR; \
+        } \
+        double b = AS_NUMBER(pop()); \
+        double a = AS_NUMBER(pop()); \
+        push(value_type(a op b)); \
     } while(false)
 
     for (;;) {
@@ -54,9 +80,8 @@ static InterpretResult run() {
         switch (instruction = READ_BYTE()) {
         case OP_CONSTANT: {
             Value constant = READ_CONSTANT();
-            push(constant);
-            break;
-        }
+            push(constant);           
+        }  break;
         case OP_CONSTANT_LONG: {
             uint16_t constant_id;
             uint8_t* constant_id_bytes = (uint8_t*)&constant_id;
@@ -65,13 +90,30 @@ static InterpretResult run() {
 
             Value constant = vm.chunk->constants.values[constant_id];
             push(constant);
-            break;
-        }
-        case OP_ADD: BINARY_OP(+); break;
-        case OP_SUBTRACT: BINARY_OP(-); break;
-        case OP_MULTILPY: BINARY_OP(*); break;
-        case OP_DIVIDE: BINARY_OP(/ ); break;
-        case OP_NEGATE: push(-pop()); break; 
+        } break;
+        case OP_NIL: push(NIL_VAL); break;
+        case OP_TRUE: push(BOOL_VAL(true)); break;
+        case OP_FALSE: push(BOOL_VAL(false)); break;
+        case OP_EQUAL: {
+            Value a = pop();
+            Value b = pop();
+            push(BOOL_VAL(valuesEqual(a, b)));
+        } break;
+        case OP_GREATER: BINARY_OP(BOOL_VAL, >); break;
+        case OP_LESS: BINARY_OP(BOOL_VAL, <); break;
+        case OP_ADD: BINARY_OP(NUMBER_VAL, +); break;
+        case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
+        case OP_MULTILPY: BINARY_OP(NUMBER_VAL, *); break;
+        case OP_DIVIDE: BINARY_OP(NUMBER_VAL, /); break;
+        case OP_NOT: 
+            push(BOOL_VAL(isFalsey(pop())));
+        case OP_NEGATE:
+            if (!IS_NUMBER(peek(0))) {
+                runtimeError("Operand must be a number.");
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            push(NUMBER_VAL(-AS_NUMBER(pop())));
+        break;
         case OP_RETURN: {
             printValue(pop());
             printf("\n");
